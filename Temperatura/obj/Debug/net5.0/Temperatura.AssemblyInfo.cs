3ac5176d2b6b124ecf786dca26f957325a7a48ea
Livia Coutinho <99189965+Livia-Coutinho@users.noreply.github.com>
2024-05-23 16:04:05 -0300

    implentando classe teste temperatura

--- Temperatura/obj/Debug/net5.0/Temperatura.AssemblyInfo.cs
+++ Temperatura/obj/Debug/net5.0/Temperatura.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Temperatura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c0499c6d53052c11f8b598becff51c4b56a033")]
-[assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Temperatura")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Temperatura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- xUnit/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ xUnit/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6fba6f561da4553d8411479f68c5645c7fc3b00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c0499c6d53052c11f8b598becff51c4b56a033")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
